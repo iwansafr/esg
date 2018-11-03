bf2fa165b50e9117662e326b8189ff1eefe91e3d
iwansafr <iwansafr@gmail.com>
2018-11-03 08:15:25 +0700

    template new age for public template

---
+++ application/modules/home/controllers/Home.php
@@ -0,0 +1,21 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('esg_model');
+		$this->load->library('esg');
+		$this->esg_model->init();
+	}
+	public function index()
+	{
+		$this->load->view('index');
+	}
+
+	public function list()
+	{
+		$this->load->view('index');
+	}
+}
--- database.sql
+++ database.sql
@@ -1,11 +1,30 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Nov 03, 2018 at 08:07 AM
+-- Server version: 5.7.23-0ubuntu0.18.04.1
+-- PHP Version: 7.2.11-2+ubuntu18.04.1+deb.sury.org+1
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Database: `esg`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comment`
+--
 
 DROP TABLE IF EXISTS `comment`;
 CREATE TABLE `comment` (
@@ -21,6 +40,12 @@ CREATE TABLE `comment` (
   `updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `config`
+--
+
 DROP TABLE IF EXISTS `config`;
 CREATE TABLE `config` (
   `id` int(11) NOT NULL,
@@ -28,10 +53,20 @@ CREATE TABLE `config` (
   `value` mediumtext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `config`
+--
+
 INSERT INTO `config` (`id`, `name`, `value`) VALUES
-(1, 'templates', '{\"public_template\":\"up_landed\",\"admin_template\":\"AdminLTE\"}'),
+(1, 'templates', '{\"public_template\":\"new_age\",\"admin_template\":\"AdminLTE\"}'),
 (2, 'site', '{\"title\":\"esoftgreat\",\"link\":\"http:\\/\\/localhost\\/esoftgreat\",\"image\":\"image_esoftgreat_1541123898.png\",\"keyword\":\"software developmet\",\"description\":\"software development and it consultanr\"}'),
 (3, 'logo', '{\"title\":\"esoftgreat\",\"image\":\"image_esoftgreat_1541123928.png\",\"width\":\"150\",\"height\":\"50\"}');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `content`
+--
 
 DROP TABLE IF EXISTS `content`;
 CREATE TABLE `content` (
@@ -58,6 +93,12 @@ CREATE TABLE `content` (
   `publish` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `content_cat`
+--
+
 DROP TABLE IF EXISTS `content_cat`;
 CREATE TABLE `content_cat` (
   `id` int(11) NOT NULL,
@@ -72,12 +113,24 @@ CREATE TABLE `content_cat` (
   `updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `content_tag`
+--
+
 DROP TABLE IF EXISTS `content_tag`;
 CREATE TABLE `content_tag` (
   `id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menu`
+--
 
 DROP TABLE IF EXISTS `menu`;
 CREATE TABLE `menu` (
@@ -91,11 +144,23 @@ CREATE TABLE `menu` (
   `publish` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menu_position`
+--
+
 DROP TABLE IF EXISTS `menu_position`;
 CREATE TABLE `menu_position` (
   `id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
 
 DROP TABLE IF EXISTS `product`;
 CREATE TABLE `product` (
@@ -115,6 +180,12 @@ CREATE TABLE `product` (
   `updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_cat`
+--
+
 DROP TABLE IF EXISTS `product_cat`;
 CREATE TABLE `product_cat` (
   `id` int(11) NOT NULL,
@@ -128,12 +199,24 @@ CREATE TABLE `product_cat` (
   `updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_tag`
+--
+
 DROP TABLE IF EXISTS `product_tag`;
 CREATE TABLE `product_tag` (
   `id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
@@ -148,8 +231,18 @@ CREATE TABLE `user` (
   `updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `user`
+--
+
 INSERT INTO `user` (`id`, `username`, `password`, `email`, `image`, `user_role_id`, `active`, `created`, `updated`) VALUES
 (1, 'root', '$2y$10$iN3I64zsXAyy9MCEVAPe3uqv1ygazlJgKFYEc2aNCiu2VDe/ZTKjO', 'root@esoftgreat.com', '', 1, 1, '2018-11-03 07:36:32', '2018-11-03 07:36:32');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_login`
+--
 
 DROP TABLE IF EXISTS `user_login`;
 CREATE TABLE `user_login` (
@@ -159,6 +252,12 @@ CREATE TABLE `user_login` (
   `status` tinyint(1) NOT NULL COMMENT '0=failed, 1=success',
   `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_role`
+--
 
 DROP TABLE IF EXISTS `user_role`;
 CREATE TABLE `user_role` (
@@ -170,9 +269,19 @@ CREATE TABLE `user_role` (
   `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `user_role`
+--
+
 INSERT INTO `user_role` (`id`, `level`, `title`, `description`, `created`, `updated`) VALUES
 (1, 1, 'root', 'super user', '2018-11-02 22:57:22', '2018-11-02 22:57:22'),
 (2, 2, 'admin', 'the administrator', '2018-11-02 22:57:22', '2018-11-02 22:57:22');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `visitor`
+--
 
 DROP TABLE IF EXISTS `visitor`;
 CREATE TABLE `visitor` (
@@ -182,78 +291,168 @@ CREATE TABLE `visitor` (
   `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Indexes for dumped tables
+--
 
+--
+-- Indexes for table `comment`
+--
 ALTER TABLE `comment`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `config`
+--
 ALTER TABLE `config`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `content`
+--
 ALTER TABLE `content`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `content_cat`
+--
 ALTER TABLE `content_cat`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id` (`id`);
 
+--
+-- Indexes for table `content_tag`
+--
 ALTER TABLE `content_tag`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `menu`
+--
 ALTER TABLE `menu`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `menu_position`
+--
 ALTER TABLE `menu_position`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `product`
+--
 ALTER TABLE `product`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `product_cat`
+--
 ALTER TABLE `product_cat`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id` (`id`);
 
+--
+-- Indexes for table `product_tag`
+--
 ALTER TABLE `product_tag`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `user`
+--
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `user_login`
+--
 ALTER TABLE `user_login`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `user_role`
+--
 ALTER TABLE `user_role`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `visitor`
+--
 ALTER TABLE `visitor`
   ADD PRIMARY KEY (`id`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
 
+--
+-- AUTO_INCREMENT for table `comment`
+--
 ALTER TABLE `comment`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `config`
+--
 ALTER TABLE `config`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `content`
+--
 ALTER TABLE `content`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `content_cat`
+--
 ALTER TABLE `content_cat`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `content_tag`
+--
 ALTER TABLE `content_tag`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `menu`
+--
 ALTER TABLE `menu`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `menu_position`
+--
 ALTER TABLE `menu_position`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `product`
+--
 ALTER TABLE `product`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `product_cat`
+--
 ALTER TABLE `product_cat`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `product_tag`
+--
 ALTER TABLE `product_tag`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `user`
+--
 ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `user_login`
+--
 ALTER TABLE `user_login`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `user_role`
+--
 ALTER TABLE `user_role`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `visitor`
+--
 ALTER TABLE `visitor`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
