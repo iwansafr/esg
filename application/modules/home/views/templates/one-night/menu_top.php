f4868f89748e75aea337963ec4ea4c8cee38e3b5
iwansafr <iwansafr@gmail.com>
2018-11-30 10:53:46 +0700

    try

--- application/modules/home/views/templates/one-night/menu_top.php
+++ application/modules/home/views/templates/one-night/menu_top.php
@@ -13,7 +13,7 @@
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Menu
+          MenuS
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
           <a class="dropdown-item" href="list.php">List</a>
