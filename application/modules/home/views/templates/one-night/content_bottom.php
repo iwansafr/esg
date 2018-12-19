86cc994edf728efb3b25efa0bf4b8ed8b999267b
iwansafr <iwansafr@gmail.com>
2018-12-19 23:28:26 +0700

    grid on one night

--- application/modules/home/views/templates/one-night/content_bottom.php
+++ application/modules/home/views/templates/one-night/content_bottom.php
@@ -12,27 +12,28 @@ if(!empty($home['content_bottom']))
 	</ul>
 	<hr>
 	<div class="card-group">
-		<?php
-		foreach ($home['content_bottom'] as $key => $value)
-		{
-
-			?>
-			<div class="col-md-4">
-			  <div class="card">
-			    <img class="img-grid card-img-top" src="<?php echo image_module('content', $value['id'].'/'.$value['image']) ?>" alt="<?php echo $value['title'] ?>">
-			    <div class="card-body">
-			    	<h5 class="card-title"><a href="<?php echo content_link($value['slug']) ?>"><?php echo $value['title'] ?></a></h5>
-			      <p style="font-size: 12px;"><?php echo $value['intro'] ?></p>
-			    </div>
-					<div class="card-footer">
-		      	<small class="text-muted">Last updated  mins ago</small>
-					</div>
-			  </div>
-				<hr>
-			</div>
+		<div class="row">
 			<?php
-		}
-		?>
+			foreach ($home['content_bottom'] as $key => $value)
+			{
+				?>
+				<div class="col-6 col-md-3">
+				  <div class="card">
+				    <img class="img-grid card-img-top" src="<?php echo image_module('content', $value['id'].'/'.$value['image']) ?>" alt="<?php echo $value['title'] ?>">
+				    <div class="card-body">
+				    	<h5 class="card-title"><a href="<?php echo content_link($value['slug']) ?>"><?php echo $value['title'] ?></a></h5>
+				      <p style="font-size: 12px;"><?php echo $value['intro'] ?></p>
+				    </div>
+						<div class="card-footer">
+			      	<small class="text-muted">created on <?php echo content_date($value['created']); ?></small>
+						</div>
+				  </div>
+					<hr>
+				</div>
+				<?php
+			}
+			?>
+		</div>
 	</div>
 	<?php
 }
