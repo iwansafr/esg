279c36cbce4c2785d4ff39e4b749539b5975c4cf
iwansafr <iwansafr@gmail.com>
2018-12-19 16:04:32 +0700

    bugfix user list

--- application/modules/admin/views/user/list.php
+++ application/modules/admin/views/user/list.php
@@ -5,8 +5,17 @@ $this->zea->init('roll');
 $this->zea->search();
 
 $this->zea->order_by('username','ASC');
+if(!is_root())
+{
+	$root_id = $this->db->get_where('user_role', 'level =1 ')->row_array();
+	if(!empty($root_id)){
+		$root_id = $root_id['id'];
+	}
+	$this->zea->setWhere('user_role_id != '.$root_id.' ');
+}else{
+	$this->zea->setWhere('id > 0');
+}
 $this->zea->setField(array('username','email'));
-$this->zea->setWhere('id > 0');
 $this->zea->setTable('user');
 $this->zea->addInput('id','plaintext');
 $this->zea->addInput('username','plaintext');
