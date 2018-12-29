8a19042f6a3bf9465d5338a675b8b34cbdcb0cbf
iwansafr <iwansafr@gmail.com>
2018-12-29 10:32:29 +0700

    size brand from col-md-2 to col

--- application/modules/home/views/templates/Avilon/content_brand.php
+++ application/modules/home/views/templates/Avilon/content_brand.php
@@ -8,7 +8,7 @@ if(!empty($home['content_brand']))
       foreach ($home['content_brand'] as $key => $value) 
       {
         ?>
-        <div class="col-md-2">
+        <div class="col">
           <!-- <img src="<?php echo image_module('content', $value['id'].'/'.$value['image'], $value['image_link']) ?>" alt=""> -->
           <img src="<?php echo image_module('content', $value) ?>" alt="">
         </div>
