5c0bdd4d5ea0d2721d0927daa35012362c63d924
iwansafr <iwansafr@gmail.com>
2018-12-30 09:02:12 +0700

    brand size

--- application/modules/home/views/templates/Avilon/content_brand.php
+++ application/modules/home/views/templates/Avilon/content_brand.php
@@ -8,7 +8,7 @@ if(!empty($home['content_brand']))
       foreach ($home['content_brand'] as $key => $value) 
       {
         ?>
-        <div class="col">
+        <div class="col-md-4">
           <!-- <img src="<?php echo image_module('content', $value['id'].'/'.$value['image'], $value['image_link']) ?>" alt=""> -->
           <img src="<?php echo image_module('content', $value) ?>" alt="">
         </div>
