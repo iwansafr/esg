fefadd88752151825bcf87519ab652cebc66e375
iwansafr <iwansafr@gmail.com>
2019-01-04 05:06:53 +0700

    new template academy

---
+++ application/modules/home/views/templates/Academy/meta.php
@@ -0,0 +1,14 @@
+<meta charset="UTF-8">
+<meta name="description" content="">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+<!-- Title -->
+<title>Academy - Education Course Template</title>
+
+<!-- Favicon -->
+<link rel="icon" href="<?php echo base_url('templates/academy')?>/img/core-img/favicon.ico">
+
+<!-- Core Stylesheet -->
+<link rel="stylesheet" href="<?php echo base_url('templates/academy')?>/style.css">
