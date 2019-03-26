8369012f8f09a6947a84cbd8cbc69ff7cf3f313d
iwansafr <iwansafr@gmail.com>
2019-03-26 08:16:01 +0700

    logo image in footer

--- application/modules/home/views/templates/academy/contact.php
+++ application/modules/home/views/templates/academy/contact.php
@@ -1,6 +1,6 @@
 <div class="footer-widget mb-100">
 	<div class="widget-title">
-		<a href="<?php echo base_url() ?>"><img src="<?php echo image_module('config', @$logo['image']) ?>" alt="<?php echo @$logo['title'] ?>" width="<?php echo $logo['width'] ?>"></a>
+		<a href="<?php echo base_url() ?>"><img src="<?php echo image_module('config/logo', @$logo['image']) ?>" alt="<?php echo @$logo['title'] ?>" width="<?php echo $logo['width'] ?>"></a>
 	</div>
 	<p><?php echo @$meta['contact']['description'] ?></p>
 	<div class="footer-social-info">
