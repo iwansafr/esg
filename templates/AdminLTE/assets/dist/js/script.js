6c1fbed01f2e2ac28760ff85900ceb2177cba5d3
iwansafr <iwansafr@gmail.com>
2019-03-27 17:01:33 +0700

    disable loading if link open new tab

--- templates/AdminLTE/assets/dist/js/script.js
+++ templates/AdminLTE/assets/dist/js/script.js
@@ -79,7 +79,11 @@ $(document).ready(function(){
 	});
 	$('a').on('click',function(){
 		var this_link = this.href;
+		var target = this.target;
 		if(this_link.includes('#')){
+
+		}else if(target=='_blank')
+		{
 
 		}else{
 			$('#loading').removeClass('hidden');
