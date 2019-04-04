d834fa43f7590dbefdd5758e2da4d17c8a2adbdb
iwansafr <iwansafr@gmail.com>
2019-04-04 12:51:00 +0700

    remove cache in admin

--- application/modules/admin/views/index.php
+++ application/modules/admin/views/index.php
@@ -1,5 +1,3 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 $this->esg->check_login();
-$this->esg->check_cache();
-$this->output->cache(60);
 $this->load->view('templates'.DIRECTORY_SEPARATOR.$this->esg->get_esg('templates')['admin_template'].DIRECTORY_SEPARATOR.'index', $this->esg->get_esg('templates'));
