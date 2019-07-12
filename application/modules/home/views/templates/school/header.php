d798d5243cb948b5201c2e41b5116da268b22e52
iwansafr <iwansafr@gmail.com>
2019-07-12 07:41:52 +0700

    surprise width and height in header

--- application/modules/home/views/templates/school/header.php
+++ application/modules/home/views/templates/school/header.php
@@ -3,7 +3,7 @@ $header = $this->esg->get_config('header');
 if(!empty($header['image']))
 {
 	?>
-	<img src="<?php echo image_module('config','header'.'/'.$header['image']) ?>" style="width: <?php echo $header['width'] ?>%;height: <?php echo $header['height'] ?>%;" class="img-fluid img" alt="">
+	<img src="<?php echo image_module('config','header'.'/'.$header['image']) ?>" style="width: <?php echo @intval($header['width']) ?>%;height: <?php echo @intval($header['height']) ?>%;" class="img-fluid img" alt="">
 	<?php
 }
 ?>
