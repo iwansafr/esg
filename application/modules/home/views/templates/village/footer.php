6989701f34aeac96e4ab84754cc215173435d263
iwansafr <iwansafr@gmail.com>
2019-07-13 13:24:19 +0700

    phone

--- application/modules/home/views/templates/village/footer.php
+++ application/modules/home/views/templates/village/footer.php
@@ -62,7 +62,12 @@
 	if(!empty($contact['wa']))
 	{
 		echo '<a href="https://wa.me/'.$contact['wa'].'?text=hai+'.@$contact['name'].'"><img src="https://www.esoftgreat.com/images/wa.png" style="height: 75px; position: fixed;bottom: 5%; right: 0.5%;z-index: 9999;"></a>';
-	}?>
+	}
+	if(!empty($contact['phone']))
+	{
+		echo '<a href="tel:'.$contact['phone'].'" class="btn btn-warning" style="height: 40px; position: fixed;bottom: 0%; right: 1.5%;z-index: 9999;"><span class="fa fa-phone"></span></a>';
+	}
+	?>
 	<br>
 	<hr>
 	<p style="margin: 0;padding: 0;height: 60px;line-height: 45px;">&copy; Copyright <?php echo date('Y') ?> All Right Reserved. <a style="color: #16a085;" href="https://www.dinusa.co.id">PT Media Nusa Perkasa</a></p>
