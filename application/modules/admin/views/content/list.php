c6e8d6cf179bd33969fa52c6ef2ae7b309b64e12
iwansafr <iwansafr@gmail.com>
2019-08-02 18:59:33 +0700

    content list datatables false

--- application/modules/admin/views/content/list.php
+++ application/modules/admin/views/content/list.php
@@ -40,7 +40,7 @@ $this->zea->setEdit(TRUE);
 $this->zea->setEditLink(base_url('admin/content/edit?id='));
 $this->zea->setUrl('admin/content/clear_list');
 $this->zea->setFormName('content_list');
-$this->zea->setDataTable();
+$this->zea->setDataTable(false);
 if(!empty($this->zea->getData()['data']))
 {
 	$this->zea->form();
