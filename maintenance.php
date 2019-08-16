95d61c87b5e27a33b752b4471ba364aa284a6855
iwansafr <iwansafr@gmail.com>
2019-08-16 20:52:51 +0700

    under construction page

---
+++ maintenance.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="description" content="">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>this website is under-construction</title>
+
+	<link rel="icon" href="images/maintenance.png">
+</head>
+<body>
+	<div class="container" style="text-align: center;">
+		<h1>Mohon Maaf Atas Gangguannya</h1>
+		<img src="images/maintenance.png" alt="" style="width: 30%;">
+	</div>
+</body>
+</html>
