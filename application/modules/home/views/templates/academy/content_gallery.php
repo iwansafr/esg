bf1ed209d16a2790482a56053f55c325c0918d10
iwansafr <iwansafr@gmail.com>
2019-10-09 01:36:00 +0700

    image size

--- application/modules/home/views/templates/academy/content_gallery.php
+++ application/modules/home/views/templates/academy/content_gallery.php
@@ -6,7 +6,7 @@
 		<div class="gallery-list d-flex justify-content-between flex-wrap">
 			<?php foreach ($home['content_gallery'] as $key => $value): ?>
 				<?php $image = image_module('content', $value) ?>
-				<a href="<?php echo $image ?>" class="gallery-img" title="<?php echo $value['title'] ?>"><img  style="object-fit: cover; width: 125px;height: 95px!important;" src="<?php echo $image;?>" alt="<?php echo $value['title'] ?>"></a>
+				<a href="<?php echo $image ?>" style="object-fit: cover; width: 125px;height: 95px!important;" class="gallery-img" title="<?php echo $value['title'] ?>"><img style="object-fit: cover; width: 125px;height: 95px!important;" src="<?php echo $image;?>" alt="<?php echo $value['title'] ?>"></a>
 			<?php endforeach ?>
 		</div>
 	</div>
