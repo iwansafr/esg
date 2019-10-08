9235927f5a98dcbbf1674bafbf0d6001442318cf
iwansafr <iwansafr@gmail.com>
2019-10-09 01:02:33 +0700

    surprise home on twitter

--- application/modules/home/views/templates/magazine/twitter.php
+++ application/modules/home/views/templates/magazine/twitter.php
@@ -1,6 +1,6 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');?>
 <h4>Twitter</h4>
 <div class="fslider customjs testimonial twitter-scroll twitter-feed" data-username="envato" data-count="2" data-animation="slide" data-arrows="false">
-	<a class="twitter-timeline" href="https://twitter.com/<?php echo $home['twitter_widget']['content'] ?>" data-height="350">Tweets by <?php echo $home['twitter_widget']['content'] ?></a> 
+	<a class="twitter-timeline" href="https://twitter.com/<?php echo @$home['twitter_widget']['content'] ?>" data-height="350">Tweets by <?php echo @$home['twitter_widget']['content'] ?></a> 
 	<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 </div>
