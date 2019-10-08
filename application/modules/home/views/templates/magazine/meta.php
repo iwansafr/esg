f8a88fe45906340195aa2060961c9bb262060e47
iwansafr <iwansafr@gmail.com>
2019-10-09 01:09:26 +0700

    ssl googlfont

--- application/modules/home/views/templates/magazine/meta.php
+++ application/modules/home/views/templates/magazine/meta.php
@@ -1,7 +1,7 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');?>
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 
-<link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
+<link href="https://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
 <link rel="stylesheet" href="<?php echo $link_template;?>/css/bootstrap.css" type="text/css" />
 <link rel="stylesheet" href="<?php echo $link_template;?>/style.css" type="text/css" />
 <link rel="stylesheet" href="<?php echo $link_template;?>/css/dark.css" type="text/css" />
