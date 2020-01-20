ccb4a6fdfa5bace5d73bc55f24496a9794d47453
iwansafr <iwansafr@gmail.com>
2020-01-21 04:29:28 +0700

    link zea

--- application/libraries/ZEA/input/plaintext.php
+++ application/libraries/ZEA/input/plaintext.php
@@ -12,6 +12,23 @@ if(!empty($field))
 			echo form_label(ucfirst($label), $label).' '.@$this->help[$field];
 			$data_value = $this->value[$field];
 		}
+		if(!empty($this->plaintext[$field])){
+			$data_value = $this->plaintext[$field];
+			if(preg_match_all('~{.*?}~', $data_value, $output))
+			{
+				// pr($output);
+			}
+			if(!empty($output))
+			{
+				foreach ($output[0] as $output_key => $output_value) 
+				{
+					$tmp_output_value; $output_value;
+					$tmp_output_value = str_replace('{', '', $output_value);
+					$tmp_output_value = str_replace('}', '', $tmp_output_value);
+					$data_value = preg_replace('~'.$output_value.'~', $data[$tmp_output_value], $data_value);
+				}
+			}
+		}
 		echo '<br>';
 	}else{
 		$data_value = $dvalue[$ikey];
