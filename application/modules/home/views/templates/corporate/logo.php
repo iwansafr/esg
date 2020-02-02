58e8947f5153e695c5045b116dd7b533f93c9804
iwansafr <iwansafr@gmail.com>
2020-02-03 06:10:43 +0700

    bugfix logo

--- application/modules/home/views/templates/corporate/logo.php
+++ application/modules/home/views/templates/corporate/logo.php
@@ -1,6 +1,6 @@
 <div class="container clearfix">
 	<div id="logo" class="divcenter">
-		<?php if (!empty($logo[$logo['display']]) && isLink($logo[$logo['display']])): ?>
+		<?php if (!empty($logo[$logo['display']]) && $logo['display'] == 'image'): ?>
 			<a href="<?php echo $site['link'] ?>" class="standard-logo" data-dark-logo="<?php echo image_module('config', 'logo/'.$logo['image']) ?>"><img class="divcenter" src="<?php echo image_module('config', 'logo/'.$logo['image']) ?>" alt="<?php echo $logo['title'] ?> Logo"></a>
 			<a href="<?php echo $site['link'] ?>" class="retina-logo" data-dark-logo="<?php echo image_module('config', 'logo/'.$logo['image']) ?>"><img class="divcenter" src="<?php echo image_module('config', 'logo/'.$logo['image']) ?>" alt="<?php echo $logo['title'] ?> Logo"></a>
 		<?php else: ?>
