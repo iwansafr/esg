1c5ce32967ddf66e923aebea2918ac5c74110073
iwansafr <iwansafr@gmail.com>
2020-02-10 10:09:49 +0700

    mega menu

--- application/modules/home/views/templates/corporate_mega/menu_main.php
+++ application/modules/home/views/templates/corporate_mega/menu_main.php
@@ -4,38 +4,33 @@
 			<?php if (!empty($home['menu_main'])): ?>
 				<div id="primary-menu-trigger"><i class="icon-reorder"></i></div>
 				<ul>
+					<li>
+						<?php $this->load->view('logo') ?>
+					</li>
 					<?php foreach ($home['menu_main'] as $key => $value): ?>
 						<?php if (empty($value['child'])): ?>
 							<li><a href="<?php echo menu_link($value['link']) ?>"><?php echo $value['title'] ?></a></li>
 						<?php else: ?>
 							<li class="mega-menu"><a href="#"><div><?php echo $value['title'] ?></div></a>
 								<div class="mega-menu-content style-2 clearfix">
-									<ul>
-										<?php foreach ($value['child'] as $ckey => $cvalue): ?>
+									<?php foreach ($value['child'] as $ckey => $cvalue): ?>
+										<ul class="mega-menu-column col-md-3">
 											<?php if (empty($cvalue['child'])): ?>
 												<li><a href="<?php echo menu_link($cvalue['link']) ?>"><?php echo $cvalue['title'] ?></a></li>								
 											<?php else: ?>
-												<li><a href="#"><div><i class="icon-stack"></i><?php echo $cvalue['title'] ?></div></a>
+												<li class="mega-menu-title"><a href="#"><div><i class="icon-stack"></i><?php echo $cvalue['title'] ?></div></a>
 													<ul>
 														<?php foreach ($cvalue['child'] as $gckey => $gcvalue): ?>
 															<?php if (empty($gcvalue['child'])): ?>
 																<li><a href="<?php echo menu_link($gcvalue['link']) ?>"><div><?php echo $gcvalue['title'] ?></div></a>
-															<?php else: ?>
-																<li><a href="<?php echo menu_link($gcvalue['link']) ?>"><div><?php echo $gcvalue['title'] ?></div></a>
-																	<ul>
-																		<?php foreach ($gcvalue['child'] as $ggckey => $ggcvalue): ?>
-																			<li><a href="<?php echo menu_link($ggcvalue['link']) ?>"><div><?php echo $ggcvalue['title'] ?></div></a></li>
-																		<?php endforeach ?>
-																	</ul>
-																</li>
 															<?php endif ?>
 														<?php endforeach ?>
 													</ul>
 												</li>
 											<?php endif ?>	
-										<?php endforeach ?>
-									</ul>
-								</div>
+										</ul>
+									<?php endforeach ?>
+								</li>
 							</li>
 						<?php endif ?>
 					<?php endforeach ?>
