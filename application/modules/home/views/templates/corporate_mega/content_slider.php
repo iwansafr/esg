fae64b63d75b4977e0c3375d544007819a8735ff
iwansafr <iwansafr@gmail.com>
2020-02-17 18:13:48 +0700

    remove roll in slider

--- application/modules/home/views/templates/corporate_mega/content_slider.php
+++ application/modules/home/views/templates/corporate_mega/content_slider.php
@@ -1,6 +1,6 @@
 <?php if (!empty($home['content_slider'])): ?>
 	<section id="slider" class="slider-parallax" style="background-color: #222;">
-		<div id="oc-slider" class="owl-carousel carousel-widget" data-margin="0" data-items="1" data-pagi="false" data-loop="true" data-animate-in="rollIn" data-speed="450" data-animate-out="rollOut" data-autoplay="5000">
+		<div id="oc-slider" class="owl-carousel carousel-widget" data-margin="0" data-items="1" data-pagi="false" data-loop="true" data-speed="450" data-autoplay="5000">
 			<?php foreach ($home['content_slider'] as $key => $value): ?>
 				<a href="#"><img src="<?php echo image_module('content',$value) ?>" alt="Slider"></a>
 			<?php endforeach ?>
