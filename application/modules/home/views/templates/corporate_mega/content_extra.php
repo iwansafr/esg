6180cc2ff1c4f76ab5bb38af9fa0f04379ec1716
iwansafr <iwansafr@gmail.com>
2020-03-09 06:13:50 +0700

    margin top content extra corporate mega

--- application/modules/home/views/templates/corporate_mega/content_extra.php
+++ application/modules/home/views/templates/corporate_mega/content_extra.php
@@ -9,7 +9,7 @@
 			{
 				?>
 				<div class="col-md-6">
-					<div class="row" style="margin-bottom: 5%; height: 200px;">
+					<div class="row" style="margin-top: 5%;margin-bottom: 5%; height: 200px;">
 						<div class="col-md-2">
 							<i class="fa <?php echo $value['icon'] ?>" style="color: #666666;font-size: 80px;transition: 0.5s;line-height: 0;"></i>
 						</div>
