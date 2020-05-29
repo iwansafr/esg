241f10851bf1743f83b845d7d73fbc085ea1fae3
iwansafr <iwansafr@gmail.com>
2020-05-29 16:29:28 +0700

    border

--- application/modules/home/views/templates/property/content/default_detail.php
+++ application/modules/home/views/templates/property/content/default_detail.php
@@ -65,7 +65,7 @@ if(!empty($content))
 									}
 									?>
 									<?php foreach ($child_images as $ckey => $cvalue): ?>
-										<div class="col-md-6">
+										<div class="col-md-6" style="border: 1px solid grey;">
 											<a href="<?php echo $cvalue['image_link'] ?>" data-lightbox="gallery-item"><img class="image_fade" style="object-fit: cover; width: 100%; height: 250px;" src="<?php echo $cvalue['image_link'] ?>" alt="Gallery Thumb 1"></a>
 										</div>
 									<?php endforeach ?>
