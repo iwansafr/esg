4c48009a9d6ebdb33488747e91e764e24c4c46d8
iwansafr <iwansafr@gmail.com>
2020-05-30 17:47:50 +0700

    if contnt empty on detail content property

--- application/modules/home/views/templates/property/content/detail.php
+++ application/modules/home/views/templates/property/content/detail.php
@@ -3,5 +3,13 @@ if(!empty($content['tpl']))
 {
 	$this->load->view($content['tpl'],['content'=>$content]);
 }else{
-	$this->load->view('default_detail',['content'=>$content]);	
+	if(!empty($content))
+	{
+		$this->load->view('default_detail',['content'=>$content]);	
+	}else{
+		echo '<br>';
+		echo '<div class="container">';
+		msg('Halaman Tidak ditemukan','danger');
+		echo '</div>';
+	}
 }
