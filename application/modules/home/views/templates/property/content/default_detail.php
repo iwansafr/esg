4e94c7ee7fdab3293088f20bf8348bb76c02af54
iwansafr <iwansafr@gmail.com>
2020-06-06 16:20:39 +0700

    padding and 2 col on detail

--- application/modules/home/views/templates/property/content/default_detail.php
+++ application/modules/home/views/templates/property/content/default_detail.php
@@ -66,8 +66,8 @@ if(!empty($content))
 									}
 									?>
 									<?php foreach ($child_images as $ckey => $cvalue): ?>
-										<div class="col-md-6" >
-											<a href="<?php echo $cvalue['image_link'] ?>" data-lightbox="gallery-item"><img class="image_fade" style="object-fit: cover; width: 100%; height: 250px; border: 1px solid grey; padding: 0 2%;" src="<?php echo $cvalue['image_link'] ?>" alt="Gallery Thumb 1"></a>
+										<div class="col-xs-6" style="padding-left: 0;">
+											<a href="<?php echo $cvalue['image_link'] ?>" data-lightbox="gallery-item"><img class="image_fade" style="object-fit: cover; width: 100%; height: 250px;" src="<?php echo $cvalue['image_link'] ?>" alt="Gallery Thumb 1"></a>
 										</div>
 									<?php endforeach ?>
 									<?php echo $value['content'] ?>
